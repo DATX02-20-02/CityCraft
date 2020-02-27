bd613eb26f65f50ed0889842fd54a78fa8af14c3
Guthiix <marcusansamaa@outlook.com>
2020-02-27 16:28:47 +0100

    Adjust code to Definition of Done

--- CityPCG-unity/Assets/TerrainGenerator/TerrainGenerator.cs
+++ CityPCG-unity/Assets/TerrainGenerator/TerrainGenerator.cs
@@ -1,100 +1,73 @@
 ﻿using UnityEngine;
 
+// What: The generator that creates and designs the landscape on which the city is built upon. 
+/* Why : For realistic reasons, mountains and oceans are also part of nature, not only flat ground.
+           The city roads needs to adjust to the landscape not the other way around*/
+// How : Perlin Noise is the main tool to generate the landscape. 
+
 public class TerrainGenerator : MonoBehaviour
 {
-    public int depth = 50;
-    public int width = 256;
-    public int height = 256;
-    public float scale = 5f;
-    public float offsetX; //= 0f;
-    public float offsetY; //= 0f;
-    public float frequency;
+    private int depth = 50;
+    private int width = 256;
+    private int height = 256;
+    private float scale = 20f;
+    private float offsetX = 0f;
+    private float offsetY = 0f;
+    private float frequency; // UNUSED
 
-    public AnimationCurve terrHeightCurve;
 
-    void Start ()
-    {
-        depth = 70;
-        offsetY = 0;
-        offsetX = 0; 
-        scale = 20f;
-        frequency = 2;
+    // Unity's base Start command. UNUSED FOR NOW
+    void Start (){ 
     }
     
-    void Update() 
-    {
+    // Unity's base Update command. Allows random generation of terrain noise. ONLY FOR DEBUGGING
+    void Update() {
         Terrain terrain = GetComponent<Terrain>();
         terrain.terrainData = GenerateTerrain(terrain.terrainData);
         
-        offsetX += Time.deltaTime*5;
+        if (Input.GetKey("u")){ // Press U to generate new terrain.
+            offsetX = Random.Range(0f,10000f);
+            offsetY = Random.Range(0f,10000f);
+        }
     }
     
-    TerrainData GenerateTerrain(TerrainData terrainData)
-    {
+    private TerrainData GenerateTerrain(TerrainData terrainData){
         terrainData.heightmapResolution = width + 1;
-        
         terrainData.size = new Vector3(width, depth, height);
-        
         terrainData.SetHeights(0,0,GenerateHeights());
         return terrainData;
     }
     
-    float[,] GenerateHeights ()
-    {
+    // For each point in the terrain plane, calculate the height for that point.
+    private float[,] GenerateHeights (){
         float[,] heights = new float [width, height];
-        for(int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                heights[x, y] = terrHeightCurve.Evaluate(CalculateHeight(x, y)); //PERLIN NOISE
-
+        //SimplexNoiseGenerator sng = new SimplexNoiseGenerator();
+        for(int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                heights[x, y] = /*terrHeightCurve.Evaluate*/(CalculateHeight(x, y)); //PERLIN NOISE
             }
         }
-        return heights;
-        
+        return heights;   
     }
     
-    float CalculateHeight(int x, int y)
-    {
-
+    private float CalculateHeight(int x, int y){
 
         float xCoord = (float)x/width*scale+offsetX;
         float yCoord = (float)y/height*scale+offsetY;
 
-        float noise = perlinfunc (xCoord,yCoord,0.9f,0.1f);
-        float noise2 = perlinfunc (xCoord,yCoord,0.2f,0.5f);
+        float noiseBase = PerlinFunc (xCoord,yCoord,0.9f,0.1f);
+        float noiseMountain= PerlinFunc (xCoord,yCoord,0.3f,0.5f);
 
-
-        if (noise > 0.6f) { //for high ground
-            float t = Mathf.InverseLerp(0.6f, 0.9f, noise);
+        if (noiseBase > 0.6f) { // High ground Perlin Noise
+            float t = Mathf.InverseLerp(0.6f, 0.9f, noiseBase);
             //float t = (Mathf.Clamp(noise,0.6f,0.8f)-0.6f)/(0.8f-0.6f);
-            return noise+(t) * noise2;
+            return noiseBase + (t) * noiseMountain;
         }
-
-
-        return noise;
+        return noiseBase;
     }
 
-    float perlinfunc(float x, float y, float con, float amp)
-    {
+    // Helper function for generating perlin noise. Takes in x & y coords, constant con to multiply the noise and amp to amplify the coord values.
+    private float PerlinFunc(float x, float y, float con, float amp){
         return con*Mathf.PerlinNoise(amp*x,amp*y);
-        
-    }
-    
-
-    float power(float x, float y)
-    {
-    float ans = 1;
-
-    for(int i=0; i<y; i++)
-      ans *= x;
-
-    return ans;
-    }
-
-    float max (float x, float y)
-    {
-        if (x > y) return x;
-        return y;
     }
 }

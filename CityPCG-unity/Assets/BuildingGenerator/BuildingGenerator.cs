298d33b23cd5c8bd312421c9757ce859ec349dca
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-02-28 11:47:17 +0100

    Lint project

--- CityPCG-unity/Assets/BuildingGenerator/BuildingGenerator.cs
+++ CityPCG-unity/Assets/BuildingGenerator/BuildingGenerator.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingGenerator : MonoBehaviour
-{
+public class BuildingGenerator : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
--- CityPCG-unity/Assets/TerrainGenerator/TerrainGenerator.cs
+++ CityPCG-unity/Assets/TerrainGenerator/TerrainGenerator.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainGenerator : MonoBehaviour
-{
+public class TerrainGenerator : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }

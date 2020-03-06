ccc8e9626d4a28bfd88935802fbd22e4a9a5bb4d
Theodor Angergård <portalprogramming@gmail.com>
2020-03-06 11:56:35 +0100

    First version of plot divider

---
+++ CityPCG-unity/Assets/Utils/PolygonSplitter/Polygon.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static Utils.PolygonSplitter.PolygonUtils;
+
+namespace Utils.PolygonSplitter
+{
+    [Serializable]
+    public class Polygon
+    {
+        public List<Vector3> points;
+        public Polygon(List<Vector3> points)
+        {
+            this.points = points;
+        }
+
+        public float GetArea()
+        {
+            var result = Vector3.zero;
+            for(int p = points.Count-1, q = 0; q < points.Count; p = q++) {
+                result += Vector3.Cross(points[q], points[p]);
+            }
+            result *= 0.5f;
+            
+            return result.magnitude;
+            //return GeometryUtility.CalculateBounds(points, Matrix4x4.identity);
+        }
+        
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("[" + GetArea() + "] ");
+
+            for (var i = 0; i < points.Count; i++)
+            {
+                var point = points[i];
+                sb.Append(point);
+
+                if (i + 1 < points.Count)
+                {
+                    sb.Append(" ---> ");
+                }
+            }
+            return sb.ToString();
+        }
+
+        //https://stackoverflow.com/a/4833823
+        public bool Contains(Polygon polygon)
+        {
+            var p1Segments = GetLineSegments(this);
+            var p2Segments = GetLineSegments(polygon);
+
+            foreach (var ls1 in p1Segments)
+            {
+                foreach (var ls2 in p2Segments)
+                {
+                    if (LineLineIntersection(ls1, ls2))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            foreach (var line in p2Segments)
+            {
+                if (Contains(line.end))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+        
+        public bool Contains(Vector3 p)
+        {
+            foreach (var point in points)
+            {
+                if (point == p)
+                {
+                    return true;
+                }
+            }
+            
+            var j = points.Count - 1;
+            var inside = false;
+            for (var i = 0; i < points.Count; j = i++)
+            {
+                var pi = points[i];
+                var pj = points[j];
+                if (((pi.z <= p.z && p.z < pj.z) || (pj.z <= p.z && p.z < pi.z)) &&
+                    (p.x < (pj.x - pi.x) * (p.z - pi.z) / (pj.z - pi.z) + pi.x))
+                    inside = !inside;
+            }
+            return inside;
+        }
+        
+        //TODO: Optimize
+        public Polygon Difference(Polygon otherPolygon)
+        {
+            var vertices = new List<Vector3>();
+
+            var thisIndex = 0;
+            var otherIndex = -1;
+	
+            for(; thisIndex < points.Count && otherIndex == -1; thisIndex++)
+            {
+                var ls = new LineSegment(points[thisIndex], points[(thisIndex + 1) % points.Count]);
+                for(var l = 0; l < otherPolygon.points.Count; l++)
+                {
+                    if(ls.start.Equals(otherPolygon.points[l]))
+                    {
+                        vertices.Add(ls.start);
+                        otherIndex = l;
+                        break;
+                    }
+			
+                    if(IsPointOnLineSegmentExcludingEndpoints(otherPolygon.points[l], ls))
+                    {
+                        vertices.Add(ls.start);
+                        vertices.Add(otherPolygon.points[l]);
+                        otherIndex = l;
+                        break;
+                    }
+                }
+            }
+	
+            otherIndex++;
+	
+            while(otherPolygon.points[otherIndex].Equals(points[thisIndex]))
+            {
+                thisIndex++;
+                otherIndex++;
+		
+                thisIndex %= points.Count;
+                otherIndex %= otherPolygon.points.Count;
+            }
+
+            vertices.Add(otherPolygon.points[otherIndex]);
+
+            //Adds last bit
+            while (!vertices.Contains(points[thisIndex]))
+            {
+                vertices.Add(points[thisIndex]);
+                thisIndex++;
+                thisIndex %= points.Count;
+            }
+
+            vertices.Add(vertices[0]);
+	
+            return new Polygon(vertices);	
+        }
+        
+    }
+}
---
+++ CityPCG-unity/Assets/Utils/PolygonSplitter/PolygonSplitter.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static Utils.PolygonSplitter.PolygonUtils;
+
+namespace Utils.PolygonSplitter
+{
+    public static class PolygonSplitter
+    {
+
+        public static List<Polygon> Split(Polygon originalPolygon, int parts)
+        {
+            var singlePartArea = originalPolygon.GetArea() / parts;
+            
+            var polygonParts = new List<Polygon>(parts);
+            var remainingPoly = originalPolygon;
+            for (var i = 0; i < parts - 1; i++)
+            {
+                remainingPoly = Split(remainingPoly, polygonParts, singlePartArea);
+            }
+            polygonParts.Add(remainingPoly);
+            
+            return polygonParts;
+        }
+
+        private static Polygon Split(Polygon polygon, List<Polygon> resultList, float singlePartArea)
+        {
+            var segments = GetLineSegments(polygon);
+            if (segments.Count < 3)
+            {
+                Debug.Log(polygon);
+                foreach (var segment in segments)
+                {
+                    Debug.Log(segment);
+                }
+                return polygon;
+            }
+            
+            var possibleCuts = new List<Cut>();
+
+            // for each unique edge pair
+            for (var i = 0; i < segments.Count - 2; i++) {
+
+                // generate unique edge pairs (e.g. 2 pairs for any rectangle)
+                for (var j = i + 2; j < segments.Count; j++) {
+                    var segmentsCovered = j - i + 1;            // number of segments covered by a LineRing starting with edgeA and ending with edgeB (including)
+                    if (segments.Count == segmentsCovered) {
+                        break;
+                    }
+
+                    var edgeA = segments[i];
+                    var edgeB = segments[j];
+                    var edgePair = new EdgePair(edgeA, edgeB);
+                    var subPolygons = edgePair.GetSubPolygons();
+                    var cutForCurrentEdgePair = subPolygons.GetCuts(polygon, singlePartArea);
+                    possibleCuts.AddRange(cutForCurrentEdgePair);
+                }
+            }
+
+            Cut shortestCut = null;
+            if (possibleCuts.Count == 0)
+            {
+                var edgeA = segments[0];
+                var edgeB = segments[2];
+                var edgePair = new EdgePair(edgeA, edgeB);
+                var subPolygons = edgePair.GetSubPolygons();
+                var cuts = subPolygons.GetCuts(polygon, singlePartArea);
+
+                if (cuts.Count == 0)
+                {
+                    return polygon;
+                }
+                else
+                {
+                    shortestCut = cuts[0];
+                }
+            }
+            else
+            {
+                Debug.Log("Possible cuts:");
+
+                shortestCut = possibleCuts[0];
+                for (var i = 1; i < possibleCuts.Count; i++)
+                {
+                    Debug.Log(possibleCuts[i]);
+                    if (possibleCuts[i].length < shortestCut.length && possibleCuts[i].cutAway != null && polygon.Contains(possibleCuts[i].cutAway))
+                    {
+                        shortestCut = possibleCuts[i];
+                    }
+                }
+            }
+            
+            resultList.Add(shortestCut.cutAway);
+            return polygon.Difference(shortestCut.cutAway);
+        }
+
+    }
+}

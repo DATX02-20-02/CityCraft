fb98c9aba22b63732a43ef39c22c66de7d31d66d
Theodor Angergård <portalprogramming@gmail.com>
2020-03-06 11:56:35 +0100

    First version of plot divider

---
+++ CityPCG-unity/Assets/Utils/PolygonSplitter/IntersectionPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using static Utils.PolygonSplitter.PolygonUtils;
+
+namespace Utils.PolygonSplitter
+{
+    public class IntersectionPosition
+    {
+        public readonly bool belongsToOneOfTheEdges;
+        public Vector3 vector;
+        
+        public IntersectionPosition(Vector3 vector, LineSegment edgeA, LineSegment edgeB)
+        {
+            this.vector = vector;
+            belongsToOneOfTheEdges = IsPointOnLineSegmentExcludingEndpoints(vector, edgeA) || IsPointOnLineSegmentExcludingEndpoints(vector, edgeB);
+        }
+
+    }
+}
---
+++ CityPCG-unity/Assets/Utils/PolygonSplitter/ProjectedVertex.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
+
+namespace Utils.PolygonSplitter
+{
+    public class ProjectedVertex 
+    {
+        public readonly LineSegment edge;
+        public readonly bool valid;
+        public readonly Vector3 vertex;
+
+        public static readonly ProjectedVertex INVALID = new ProjectedVertex();
+
+        private ProjectedVertex()
+        {
+            this.valid = false;
+            this.edge = null;
+            this.vertex = Vector3.zero;
+        }
+
+        public ProjectedVertex(Vector3 vertex, LineSegment edge)
+        {
+            this.edge = edge;
+            this.valid = true;
+            this.vertex = vertex;
+        }
+        
+        public bool IsOnEdge(LineSegment edge) {
+            return valid && this.edge.EqualsTopo(edge);
+        }
+        
+    }
+}

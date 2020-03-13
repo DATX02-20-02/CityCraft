606c8a26c228d5d0676a14028ab6dce5b78b811d
Marcus Ansamaa <marcusansamaa@outlook.com>
2020-03-13 15:32:19 +0100

    Apply Definition of Done

--- CityPCG-unity/Assets/CameraMovement.cs
+++ CityPCG-unity/Assets/CameraMovement.cs
@@ -2,54 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
+// What: A Class that applies camera control to the application.
+// Why: To allow inspecting the city from different angles and perspectives.
+// How: By scanning keyboard strokes using unity's built in Update function using standard WASD movement with rotation dependency.
+
+public class CameraMovement : MonoBehaviour{
     //public Transform cam;
-    public float mouseSensitivity = 150f;
+    [SerializeField] float mouseSensitivity = 200f;
     float xRotation = 0f;
     float yRotation = 0f;
     Vector3 up = new Vector3(0f, 1f, 0f);
 
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
 
-        if (Input.GetKey("w"))
-        {
+        if (Input.GetKey("w")){
             transform.position += transform.forward;
         }
-        if (Input.GetKey("a"))
-        {
+        if (Input.GetKey("a")){
             transform.position += transform.right * (-1);
         }
-        if (Input.GetKey("s"))
-        {
+        if (Input.GetKey("s")){
             transform.position += transform.forward * (-1);
         }
-        if (Input.GetKey("d"))
-        {
+        if (Input.GetKey("d")){
             transform.position += transform.right;
         }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
+        if (Input.GetKey(KeyCode.LeftShift)){
             transform.position -= up;
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
+        if (Input.GetKey(KeyCode.Space)){
             transform.position += up;
         }
 
-        if (!Input.GetMouseButton(1))
-        { // Unlock cursor
+        if (!Input.GetMouseButton(1)){ // Unlock cursor
             Cursor.lockState = CursorLockMode.None;
         }
-        if (Input.GetMouseButton(1))
-        { // Lock cursor for 3D camera movement
+        if (Input.GetMouseButton(1)){ // Lock cursor for 3D camera movement
             Cursor.lockState = CursorLockMode.Locked; //Lock the mouse on the center
 
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; //Get the x and y movement of the mouse

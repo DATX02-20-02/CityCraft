ed20fd80908f1f8fc21e3c045003db8e533f63db
Theodor Angergård <portalprogramming@gmail.com>
2020-03-23 17:02:11 +0100

    Base plot data class

--- CityPCG-unity/Assets/PlotGenerator/PlotGenerator.cs
+++ CityPCG-unity/Assets/PlotGenerator/PlotGenerator.cs
@@ -1,15 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlotGenerator : MonoBehaviour {
+public class PlotGenerator : MonoBehaviour
+{
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
     }
+
 }

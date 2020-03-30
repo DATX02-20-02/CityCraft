3be9c0812191bde086e5f0c67af2f17142274821
Theodor Angergård <portalprogramming@gmail.com>
2020-03-30 12:57:03 +0200

    Add various lambda functions for more control of LSystem generation

--- CityPCG-unity/Assets/Utils/LSystems/LSystem.cs
+++ CityPCG-unity/Assets/Utils/LSystems/LSystem.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 
 namespace Utils.LSystems {
-    public class LSystem<T> {
-        private readonly Dictionary<T, Rules> rules = new Dictionary<T, Rules>();
+    public class LSystem<T, U> where U : class {
+        private readonly Dictionary<T, Rules> rules;
+        private Predicate<U> shouldContinue;
+
+        public LSystem() {
+            rules = new Dictionary<T, Rules>();
+            shouldContinue = u => true;
+        }
 
         public Rules CreateRules(T type) {
             if (rules.ContainsKey(type)) {
@@ -19,21 +26,43 @@ namespace Utils.LSystems {
             return typeRules;
         }
 
-        public List<T> Run(T axiom) {
+        public void ShouldContinue(Predicate<U> shouldContinue) {
+            this.shouldContinue = shouldContinue;
+        }
+
+        //TODO Validate that all values has a rule
+        public List<T> Run(T axiom, U value) {
             var result = new List<T> {axiom};
+
+            //TODO: Break out result.Last()
             while (rules[result.Last()].HasRules()) {
-                result.Add(rules[result.Last()].Next());
+                var next = rules[result.Last()].Next(value);
+                if (next == null) {
+                    break;
+                }
+
+                //TODO: Should not cast here. Problem is that .Next need to return null or something like that
+                result.Add((T) next);
+                value = rules[result.Last()].update(value);
+
+                if (!shouldContinue(value)) {
+                    break;
+                }
             }
 
             return result;
         }
 
         public class Rules {
+            internal Func<U, U> update;
+            private Predicate<U> shouldAccept;
+
             private readonly List<Rule> rules = new List<Rule>();
             private float totalProbability = 0.0f;
 
             internal Rules() {
-
+                update = u => u;
+                shouldAccept = u => true;
             }
 
             public Rules Add(float probability, T next) {
@@ -42,28 +71,41 @@ namespace Utils.LSystems {
                 return this;
             }
 
-            public T Next() {
+            public Rules ShouldAccept(Predicate<U> shouldAccept) {
+                this.shouldAccept = shouldAccept;
+                return this;
+            }
+
+            public Rules OnAccepted(Func<U, U> update) {
+                this.update = update;
+                return this;
+            }
+
+            internal object Next(U value) {
                 if (totalProbability.CompareTo(1) != 0) {
                     throw new Exception("probabilities doesn't add up to 1.0");
                 }
 
+                //TODO: Optimize this. No need for a loop.
                 Rule? nextRule = null;
                 var d = Random.Range(0.0f, 1.0f);
                 var step = 0.0f;
                 foreach (var currentRule in rules) {
                     step += currentRule.probability;
 
-                    if (d <= step) {
+                    //TODO: Don't run accepted every time.
+                    //TODO: If an accepted is false, it will always take the next one in line.
+                    if (d <= step && shouldAccept(value)) {
                         nextRule = currentRule;
                         break;
                     }
                 }
 
                 if (!nextRule.HasValue) {
-                    throw new Exception("next rule wasn't found");
+                    return null;
                 }
 
-                return nextRule.Value.nextFloorType;
+                return nextRule.Value.nextType;
             }
 
             public bool HasRules() {
@@ -72,11 +114,11 @@ namespace Utils.LSystems {
 
             private struct Rule {
                 public readonly float probability;
-                public readonly T nextFloorType;
+                public readonly T nextType;
 
-                public Rule(float probability, T nextFloorType) {
+                public Rule(float probability, T nextType) {
                     this.probability = probability;
-                    this.nextFloorType = nextFloorType;
+                    this.nextType = nextType;
                 }
             }
         }

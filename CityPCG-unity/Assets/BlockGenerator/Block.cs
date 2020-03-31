e9abac08998af213a403e903fe127d98efcfb627
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-03-31 09:20:57 +0200

    Patch with master

--- CityPCG-unity/Assets/BlockGenerator/Block.cs
+++ CityPCG-unity/Assets/BlockGenerator/Block.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public struct Block {
     public List<Vector3> vertices;
 
+    public List<Vector2> Vertices2D() {
+        List<Vector2> res = new List<Vector2>();
+        foreach (var v in vertices)
+            res.Add(new Vector2(v.x, v.z));
+        return res;
+    }
+
     public Block(List<Vector3> vertices) {
         this.vertices = vertices;
     }
